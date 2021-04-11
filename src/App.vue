364da309dcf7fadecab9036befc59e03d4fe170a
Dennis Kawurek <dennis.kawurek@hotmail.de>
2021-04-11 11:30:31 +0200

    move styling

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,40 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.alert {
+  border-color:yellow;
+  background-color: rgb(243, 243, 171);
+  padding: 10px;
+}
+
+input,
+input:focus {
+  font-size: 2.5em;
+  text-align: center;
+  border-style: solid;
+  color: #000000;
+}
+
+input.success {
+  border-color: #4bda99;
+}
+input.error {
+  border-color: #da4b52;
+}
 </style>
